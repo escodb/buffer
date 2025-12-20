f6940d0748cd0148914d7651986157b3c2dd7b70
James Coglan <jcoglan@gmail.com>
2025-12-20 12:01:55 +0000

    Test that all parts of the base64 charset decode properly

--- test/buffer_test.js
+++ test/buffer_test.js
@@ -14,18 +14,25 @@ function assertBuffer (buf, bytes) {
   assert(bytes.every((b, i) => b === buf[i]))
 }
 
+const HELLO_WORLD = [0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64]
+
 describe('Buffer', () => {
   describe('base64', () => {
     it('encodes to base64', () => {
-      let bytes = [0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64]
-      let buf = Buffer.from(bytes)
+      let buf = Buffer.from(HELLO_WORLD)
       assert.equal(buf.toString('base64'), 'aGVsbG8gd29ybGQ=')
     })
 
-    it('decodes from base64', () => {
+    it('decodes a string from base64', () => {
       let buf = Buffer.from('aGVsbG8gd29ybGQ=', 'base64')
-      let bytes = [0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64]
+      assertBuffer(buf, HELLO_WORLD)
+    })
+
+    it('decodes arbitrary bytes from base64', () => {
+      let buf = Buffer.from('ACXZ+bgwy/019w==', 'base64')
+      let bytes = [0x00, 0x25, 0xd9, 0xf9, 0xb8, 0x30, 0xcb, 0xfd, 0x35, 0xf7]
       assertBuffer(buf, bytes)
+      assert.equal(buf.toString('base64'), 'ACXZ+bgwy/019w==')
     })
 
     it('converts to and from base64', () => {
