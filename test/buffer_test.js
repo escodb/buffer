a81c7c8209373f7098574a15a8c6e4d3d78efd68
James Coglan <jcoglan@gmail.com>
2026-05-16 23:59:56 +0100

    Make copy() and write() methods return the right representation of how many bytes were written

--- test/buffer_test.js
+++ test/buffer_test.js
@@ -36,38 +36,38 @@ function spec (name, Buffer) {
 
       it('copies the source to the start of the target', () => {
         let target = Buffer.alloc(8)
-        source.copy(target)
+        assert.equal(source.copy(target), 5)
         assertBuffer(target, [0x12, 0x34, 0x56, 0x78, 0x9a, 0, 0, 0])
       })
 
       it('copies the source to a non-zero offset', () => {
         let target = Buffer.alloc(8)
-        source.copy(target, 2)
+        assert.equal(source.copy(target, 2), 5)
         assertBuffer(target, [0, 0, 0x12, 0x34, 0x56, 0x78, 0x9a, 0])
       })
 
       it('copies the source near the end of the target', () => {
         let target = Buffer.alloc(8)
-        source.copy(target, 5)
+        assert.equal(source.copy(target, 5), 3)
         assertBuffer(target, [0, 0, 0, 0, 0, 0x12, 0x34, 0x56])
       })
 
       it('copies a source to a smaller target', () => {
         let target = Buffer.alloc(3)
-        source.copy(target)
+        assert.equal(source.copy(target), 3)
         assertBuffer(target, [0x12, 0x34, 0x56])
       })
 
       it('copies a source to a smaller target with an offset', () => {
         let target = Buffer.alloc(3)
-        source.copy(target, 2)
+        assert.equal(source.copy(target, 2), 1)
         assertBuffer(target, [0, 0, 0x12])
       })
 
       it('copies a slice of the source', () => {
         let target = Buffer.alloc(8)
-        source.copy(target, 0, 0, 3)
-        source.copy(target, 5, 2, 4)
+        assert.equal(source.copy(target, 0, 0, 3), 3)
+        assert.equal(source.copy(target, 5, 2, 4), 2)
         assertBuffer(target, [0x12, 0x34, 0x56, 0, 0, 0x56, 0x78, 0])
       })
 
@@ -78,7 +78,7 @@ function spec (name, Buffer) {
 
       it('copies past the end of the target', () => {
         let target = Buffer.alloc(8)
-        source.copy(target, 10)
+        assert.equal(source.copy(target, 10), 0)
         assertBuffer(target, [0, 0, 0, 0, 0, 0, 0, 0])
       })
 
@@ -89,13 +89,13 @@ function spec (name, Buffer) {
 
       it('copies ending past the end of the source', () => {
         let target = Buffer.alloc(8)
-        source.copy(target, 1, 2, 6)
+        assert.equal(source.copy(target, 1, 2, 6), 3)
         assertBuffer(target, [0, 0x56, 0x78, 0x9a, 0, 0, 0, 0])
       })
 
       it('copies when the source offsets are not ordered', () => {
         let target = Buffer.alloc(8)
-        source.copy(target, 1, 4, 2)
+        assert.equal(source.copy(target, 1, 4, 2), 0)
         assertBuffer(target, [0, 0, 0, 0, 0, 0, 0, 0])
       })
     })
@@ -128,94 +128,94 @@ function spec (name, Buffer) {
       it('writes an IntXBE', () => {
         let buf = Buffer.alloc(12)
 
-        buf.writeInt8(0x56)
+        assert.equal(buf.writeInt8(0x56), 1)
         assertBuffer(buf, [0x56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt8(-0x56)
+        assert.equal(buf.writeInt8(-0x56), 1)
         assertBuffer(buf, [0xaa, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt16BE(0x1234)
+        assert.equal(buf.writeInt16BE(0x1234), 2)
         assertBuffer(buf, [0x12, 0x34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt16BE(-0x1234)
+        assert.equal(buf.writeInt16BE(-0x1234), 2)
         assertBuffer(buf, [0xed, 0xcc, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt32BE(0x12345678)
+        assert.equal(buf.writeInt32BE(0x12345678), 4)
         assertBuffer(buf, [0x12, 0x34, 0x56, 0x78, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt32BE(-0x12345678)
+        assert.equal(buf.writeInt32BE(-0x12345678), 4)
         assertBuffer(buf, [0xed, 0xcb, 0xa9, 0x88, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeBigInt64BE(0x123456789abcdef0n)
+        assert.equal(buf.writeBigInt64BE(0x123456789abcdef0n), 8)
         assertBuffer(buf, [0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0, 0, 0, 0])
 
-        buf.writeBigInt64BE(-0x123456789abcdef0n)
+        assert.equal(buf.writeBigInt64BE(-0x123456789abcdef0n), 8)
         assertBuffer(buf, [0xed, 0xcb, 0xa9, 0x87, 0x65, 0x43, 0x21, 0x10, 0, 0, 0, 0])
       })
 
       it('writes a UIntXBE', () => {
         let buf = Buffer.alloc(12)
 
-        buf.writeUInt8(0x56)
+        assert.equal(buf.writeUInt8(0x56), 1)
         assertBuffer(buf, [0x56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeUint16BE(0x1234)
+        assert.equal(buf.writeUint16BE(0x1234), 2)
         assertBuffer(buf, [0x12, 0x34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeUInt32BE(0x12345678)
+        assert.equal(buf.writeUInt32BE(0x12345678), 4)
         assertBuffer(buf, [0x12, 0x34, 0x56, 0x78, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeBigUInt64BE(0x123456789abcdef0n)
+        assert.equal(buf.writeBigUInt64BE(0x123456789abcdef0n), 8)
         assertBuffer(buf, [0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0, 0, 0, 0])
       })
 
       it('writes an IntXLE', () => {
         let buf = Buffer.alloc(12)
 
-        buf.writeInt8(0x56)
+        assert.equal(buf.writeInt8(0x56), 1)
         assertBuffer(buf, [0x56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt8(-0x56)
+        assert.equal(buf.writeInt8(-0x56), 1)
         assertBuffer(buf, [0xaa, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt16LE(0x1234)
+        assert.equal(buf.writeInt16LE(0x1234), 2)
         assertBuffer(buf, [0x34, 0x12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt16LE(-0x1234)
+        assert.equal(buf.writeInt16LE(-0x1234), 2)
         assertBuffer(buf, [0xcc, 0xed, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt32LE(0x12345678)
+        assert.equal(buf.writeInt32LE(0x12345678), 4)
         assertBuffer(buf, [0x78, 0x56, 0x34, 0x12, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeInt32LE(-0x12345678)
+        assert.equal(buf.writeInt32LE(-0x12345678), 4)
         assertBuffer(buf, [0x88, 0xa9, 0xcb, 0xed, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeBigInt64LE(0x123456789abcdef0n)
+        assert.equal(buf.writeBigInt64LE(0x123456789abcdef0n), 8)
         assertBuffer(buf, [0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12, 0, 0, 0, 0])
 
-        buf.writeBigInt64LE(-0x123456789abcdef0n)
+        assert.equal(buf.writeBigInt64LE(-0x123456789abcdef0n), 8)
         assertBuffer(buf, [0x10, 0x21, 0x43, 0x65, 0x87, 0xa9, 0xcb, 0xed, 0, 0, 0, 0])
       })
 
       it('writes a UIntXLE', () => {
         let buf = Buffer.alloc(12)
 
-        buf.writeUInt8(0x56)
+        assert.equal(buf.writeUInt8(0x56), 1)
         assertBuffer(buf, [0x56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeUInt16LE(0x1234)
+        assert.equal(buf.writeUInt16LE(0x1234), 2)
         assertBuffer(buf, [0x34, 0x12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeUInt32LE(0x12345678)
+        assert.equal(buf.writeUInt32LE(0x12345678), 4)
         assertBuffer(buf, [0x78, 0x56, 0x34, 0x12, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        buf.writeBigUInt64LE(0x123456789abcdef0n)
+        assert.equal(buf.writeBigUInt64LE(0x123456789abcdef0n), 8)
         assertBuffer(buf, [0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12, 0, 0, 0, 0])
       })
 
       it('writes at an offset', () => {
         let buf = Buffer.alloc(6)
-        buf.writeUInt32BE(0x12345678, 2)
+        assert.equal(buf.writeUInt32BE(0x12345678, 2), 6)
         assertBuffer(buf, [0, 0, 0x12, 0x34, 0x56, 0x78])
       })
 
